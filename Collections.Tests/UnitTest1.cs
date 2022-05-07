f820491fe577fbecec2c88c493ab0099b6516c89
LIREX\rtsvasileva <rtvasileva@lirex.com>
2022-05-07 16:08:14 +0300

    Refresh Tests

--- Collections.Tests/UnitTest1.cs
+++ Collections.Tests/UnitTest1.cs
@@ -26,22 +26,11 @@ namespace Collections.Tests
             Assert.AreEqual(Collection_Count, 1000000);
         }
 
-        [Test]
-        [TestCase(new int[] { 10, 20, 30, 40, 50 }, TestName = "AddAtEnd - 5 elements + 1 added")]
-        [TestCase(new int[] { 10, 20, 30, 40, 50, 60, 70 }, TestName = "AddAtEnd - 7 elements + 1 added")]
-        public void Test_Collections_InsertAtEnd(int[] values)
-        {
-            Collection<int> nums = new Collection<int>(values);
-            int Collection_Count = nums.Count;
-            nums.Add(1);
-            Assert.AreEqual(nums.Count, Collection_Count + 1);
-
-        }
 
         [Test]
         [TestCase(new int[] { 10, 20, 30, 40, 50 }, TestName = "Count and Capacity - 5 elements + 1 added")]
         [TestCase(new int[] { 10, 20, 30, 40, 50, 60, 70 }, TestName = "Count and Capacity - 7 elements + 1 added")]
-        public void Test_Collections_Count_and_Capacity(int[] values)
+        public void Count_and_Capacity(int[] values)
         {
             int Collection_Count = 0;
             int capacity = 0;
@@ -82,9 +71,24 @@ namespace Collections.Tests
         }
 
         [Test]
+        [TestCase(new int[] { 10, 20, 30, 40, 50 }, TestName = "InsertAtEnd - 5 elements + 1 added")]
+        [TestCase(new int[] { 10, 22, 33, 44, 60,80, 50 }, TestName = "InsertAtEnd - 7 elements + 1 added")]
+        public void InsertAtEnd(int[] values)
+        {
+            Collection<int> nums = new Collection<int>(values);
+            int Collection_Count = nums.Count;
+            int lastValue = nums[Collection_Count-1];
+            int insertedValue = nums[Collection_Count - 1] + 10;
+            nums.Add(lastValue + 10);
+            Assert.AreEqual(insertedValue, 60);
+            Assert.AreEqual(nums.Count, Collection_Count + 1);
+
+        }
+
+        [Test]
         [TestCase(new int[] { 10, 20, 30, 40, 50 }, TestName = "InsertWithGrowAtStart - 5 elements + 3 added")]
         [TestCase(new int[] { 10, 20, 30, 40, 50, 60, 70 }, TestName = "InsertWithGrowAtStart - 7 elements + 3 added")]
-        public void InsertWithGrow(int[] values)
+        public void InsertWithGrowAtStart(int[] values)
         {
             Collection<int> nums = new Collection<int>(values);
             int count = nums.Count;
@@ -129,7 +133,7 @@ namespace Collections.Tests
             int random_number = 0;
             if (count % 2 == 1)
             {
-                middleIndex = (count + 1) / 2;
+                middleIndex = ((count + 1) / 2) - 1;
             }
 
             if (count % 2 == 0)
@@ -138,7 +142,7 @@ namespace Collections.Tests
                 random_number = rnd.Next(lastIndexValue, 1000000);
                 nums.Add(random_number);
                 count++;
-                middleIndex = (count + 1) / 2;
+                middleIndex = ((count + 1) / 2) - 1 ;
             }
 
             min = nums[middleIndex - 1];
@@ -154,7 +158,7 @@ namespace Collections.Tests
 
         [Test]
         [TestCase(new int[] { 10, 20, 30, 40, 50 }, TestName = "Delete_At_End - 5 elements - 1 removed")]
-        public void Test_Collections_Delete_At_End(int[] values)
+        public void Delete_At_End(int[] values)
         {
             Collection<int> nums = new Collection<int>(values);
             int Collection_Count = nums.Count;
@@ -170,7 +174,7 @@ namespace Collections.Tests
         [Test]
         [TestCase(new int[] { 10, 20, 30, 40, 50 }, TestName = "Delete_At_Start - 5 elements - 1 removed")]
         [TestCase(new int[] { 10, 20, 30, 40, 50, 60, 70 }, TestName = "Delete_At_Start - 7 elements - 1 removed")]
-        public void Test_Collections_Delete_At_Start(int[] values)
+        public void Delete_At_Start(int[] values)
         {
             Collection<int> nums = new Collection<int>(values);
             int Collection_Count = nums.Count;
@@ -191,9 +195,9 @@ namespace Collections.Tests
         }
 
         [Test]
-        [TestCase(new int[] { 10, 20, 30, 40, 50 }, TestName = "Test_Collections_Delete_At_Start - 5 elements - 1 removed")]
-        [TestCase(new int[] { 10, 20, 30, 40, 60, 70, 50 }, TestName = "Test_Collections_Delete_At_Start - 7 elements - 1 removed")]
-        public void Test_Collections_Exchange_First_Last(int[] values)
+        [TestCase(new int[] { 10, 20, 30, 40, 50 }, TestName = "Exchange_First_Last - 5 elements - 1 removed")]
+        [TestCase(new int[] { 10, 20, 30, 40, 60, 70, 50 }, TestName = "Exchange_First_Last - 7 elements - 1 removed")]
+        public void Exchange_First_Last(int[] values)
         {
             Collection<int> nums = new Collection<int>(values);
             int Collection_Count = nums.Count;
@@ -202,6 +206,158 @@ namespace Collections.Tests
             nums.Exchange(0, Collection_Count - 1);
             Assert.AreEqual(nums[Collection_Count - 1], firstElement);
             Assert.AreEqual(nums[0], lastElement);
+
+        }
+
+        [Test]
+        [TestCase(new int[] { 10, 20, 30, 40, 50, 60 }, TestName = "ExchangedMiddleFirst - 6 elements")]
+        [TestCase(new int[] { 10, 20, 30, 40, 50, 60, 70 }, TestName = "ExchangedMiddleFirst - 7 elements")]
+        public void ExcganedMiddleFirst(int[] values)
+        {
+            Collection<int> nums = new Collection<int>(values);
+            int count = nums.Count;
+            int firstIndexValue = nums[0];          
+            int middleIndex = 0;
+
+            Random rnd = new Random();
+            int random_number = 0;
+
+            if (count % 2 == 1)
+            {
+                middleIndex = ((count + 1) / 2) - 1;
+            }
+
+            if (count % 2 == 0)
+            {
+                int lastIndexValue = nums[count - 1];
+                random_number = rnd.Next(lastIndexValue, 1000000);
+                nums.Add(random_number);
+                count++;
+                middleIndex = ((count + 1) / 2) - 1;
+            }
+
+            int middleIndexValue = nums[middleIndex];
+
+            nums.Exchange(0, middleIndex);
+
+            Assert.AreEqual(nums[middleIndex],firstIndexValue );
+            Assert.AreEqual(nums[0], middleIndexValue);     
+            Assert.AreEqual(nums.Count, count);
+        }
+
+
+
+
+        [Test]
+        [TestCase(new int[] { 10, 20, 30, 40, 50 }, TestName = "GetElementByIndex - 5 elements - 1 removed")]
+        [TestCase(new int[] { 10, 20, 30, 40, 50, 60, 70 }, TestName = "GetElementByIndex - 7 elements - 1 removed")]
+        public void GetElementByIndex(int[] values)
+        {
+            Collection<int> nums = new Collection<int>(values);
+            int Collection_Count = nums.Count;
+            int i = 0;
+            foreach (int value in values)
+            {
+              
+              Assert.AreEqual(value,nums[i]);
+              i++;
+                
+            }
+          
+
+        }
+
+        [Test]
+        [TestCase(new int[] { 10, 20, 30, 40, 50 }, TestName = "GetElementByInvalidIndex - 5 elements - 1 removed")]
+        [TestCase(new int[] { 10, 20, 30, 40, 50, 60, 70 }, TestName = "GetElementByInvalidIndex - 7 elements - 1 removed")]
+        public void GetElementByInvalidIndex(int[] values)
+        {
+            Collection<int> nums = new Collection<int>(values);
+            int Collection_Count = nums.Count;
+
+            Random rnd= new Random();
+            int random_number;
+            random_number = rnd.Next(0,8);
+
+            if (random_number > Collection_Count - 1 )
+
+            {
+
+                Assert.That(random_number, Is.Not.InRange(0, Collection_Count - 1));
+              //  Assert.Fail();
+
+            }
+
+            else
+            {
+                Assert.That(random_number, Is.InRange(0, Collection_Count - 1));
+
+            }
+            
+        }
+
+        [Test]
+        [TestCase(new int[] { 10, 20, 30, 40, 50 }, TestName = "InsertAtInvalidIndex - 5 elements - 1 removed")]
+        [TestCase(new int[] { 10, 20, 30, 40, 50, 60, 70 }, TestName = "InsertAtInvalidIndex - 7 elements - 1 removed")]
+        public void InsertAtInvalidIndex(int[] values)
+        {
+            Collection<int> nums = new Collection<int>(values);
+            int Collection_Count = nums.Count;
+
+            Random rnd = new Random();
+            int random_number;
+            random_number = rnd.Next(0, 8);
+
+            if (random_number > Collection_Count - 1)
+
+            {
+
+                Assert.That(random_number, Is.Not.InRange(0, Collection_Count - 1));
+                //  Assert.Fail();
+
+            }
+
+            else
+            {
+                nums.InsertAt(random_number, 25);
+                Collection_Count++;
+                int insertedValue = nums[random_number];
+                Assert.AreEqual(insertedValue, 25);
+                Assert.That(random_number, Is.InRange(0, Collection_Count - 1));
+
+            }
+
+        }
+
+        [Test]
+        [TestCase(new int[] { 10, 20, 30, 40, 50 }, TestName = "DeleteAtInvalidIndex - 5 elements - 1 removed")]
+        [TestCase(new int[] { 10, 20, 30, 40, 50, 60, 70 }, TestName = "DeleteAtInvalidIndex - 7 elements - 1 removed")]
+        public void DeleteAtInvalidIndex(int[] values)
+        {
+            Collection<int> nums = new Collection<int>(values);
+            int Collection_Count = nums.Count;
+
+            Random rnd = new Random();
+            int random_number;
+            random_number = rnd.Next(0, 8);
+
+            if (random_number > Collection_Count - 1)
+
+            {
+
+                Assert.That(random_number, Is.Not.InRange(0, Collection_Count - 1));
+                //  Assert.Fail();
+
+            }
+
+            else
+            {
+                nums.RemoveAt(random_number);
+                Collection_Count--;
+                Assert.AreEqual(nums.Count, Collection_Count);
+                Assert.That(random_number, Is.InRange(0, Collection_Count - 1));
+
+            }
 
         }
 
